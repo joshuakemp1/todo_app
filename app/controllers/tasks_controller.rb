d3530bf9ba103128b422041563be82459f0f18c9
Joshua Kemp <josh.kemp@ziplist.com>
2014-11-20 14:42:51 -0500

    Completed the first part of the todo tutorial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,15 @@ class TasksController < ApplicationController
   def new
     @tasks = Task.new
   end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to root_path
+  end
+
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :note, :completed)
+  end
 end
