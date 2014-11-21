bcb1506550efa8421736652f852be6c0ab59da23
Joshua Kemp <josh.kemp@ziplist.com>
2014-11-21 13:32:52 -0500

    Adding the ability to edit tasks on making the views better

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,15 @@
 class TasksController < ApplicationController
 
   def new
-    @tasks = Task.new
+    @task = Task.new
     render :show_form
   end
 
   def create
     @task = Task.create(task_params)
     @tasks = Task.all
+
+    render :hide_form
   end
 
   def destroy
@@ -20,6 +22,16 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     render :show_form
   end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    @tasks = Task.all
+
+    render :hide_form
+  end
+
+
 
   private
   def task_params
